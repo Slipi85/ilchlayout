9dc9eea2a6fdbfb0ec1113d5bdc85c89b256a0b0
Slipi85 <52356917+Slipi85@users.noreply.github.com>
2019-12-31 15:37:32 +0100

    Update

--- application/layouts/ilchlayout/config/config.php
+++ application/layouts/ilchlayout/config/config.php
@@ -6,10 +6,12 @@ class Config extends \Ilch\Config\Install
 {
     public $config = [
         'name' => 'Ilch-Layout',
-        'version' => '1.0',
-        'author' => 'Ilch.de',
+        'version' => '1.2.1',
+        'author' => 'Slipi',
         'link' => 'http://ilch.de',
-        'desc' => 'Ilch-Layout mit Startseite',
+        'desc' => 'Ilch-Layout mit Startseite. Die Sidebar-Ausgabe im Forum wird unter Menü1 gesteuert. Die Sidebar-Ausgabe im Forum wird unter Menü2 gesteuert.
+                  Die Navigations-ausgabe wird unter Menü3 gesteuert. Die Sidebar-Ausgabe in denn restlichen Modulen wird unter Menü4 gesteuert.
+                  Die Footer-Ausgabe wird unter Menü5 gesteuert (es können nur 4 Berieche hinzugefühgt werden).',
         'layouts' => [
             'start' => [
                 ['module' => 'shoutbox'],
